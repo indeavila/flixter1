ab7b35de6863455838fd028e1e13d5ad2cc3c485
Ivan Nikolai De Avila <indeavila@gmail.com>
2019-07-17 22:10:57 +0000

    201907170510

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,24 +1,38 @@
 class Instructor::SectionsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :require_authorized_for_current_course
-    def new
-        @section = Section.new
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
+  def new
+    @section = Section.new
+  end
+  def create
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
+  end
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
+  end
+  private
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
     end
-    def create
-        @section = current_course.sections.create(section_params)
-        redirect_to instructor_course_path(current_course)
+  end
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
     end
-    private
-    def require_authorized_for_current_course
-        if current_course.user != current_user
-          render plain: "Unauthorized", status: :unauthorized
-        end
-    end    
-    helper_method :current_course
-    def current_course
-        @current_course ||= Course.find(params[:course_id])
+  end    
+  helper_method :current_course
+  def current_course
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
     end
-    def section_params
-        params.require(:section).permit(:title)
-    end
+  end
+  def section_params
+    params.require(:section).permit(:title, :row_order_position)
+  end
 end
